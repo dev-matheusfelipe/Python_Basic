613184c4cbc62e384e8eead686f0e514d3447fee
Matheus Felipe <64490699+MatheusFellype2016@users.noreply.github.com>
2022-04-05 23:02:06 -0300

    Create aula_1.py

---
+++ aula_1.py
@@ -0,0 +1,30 @@
+print('Hello World!')
+
+print('Apredendo os comando para \n mudar de linha!')
+
+print('Apredendo o comando \t para ter um espaço.')
+
+
+
+nome = 'Guilherme'
+print(nome)
+
+
+tipo = type(nome)
+print(nome) 
+
+
+nome = 'Matheus'
+idade = 25
+altura = 1.70
+tipo_nome = type(nome)
+tipo_idade = type(idade)
+tipo_altura = type(altura)
+
+print(nome)
+print(idade)
+print(tipo_nome)
+print(tipo_idade)
+print(tipo_altura)
+
+print(nome, 'tem', idade, 'anos', 'e', altura, 'de altura.' )
